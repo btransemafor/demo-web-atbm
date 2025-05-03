2735556086958b5f2afed23a73e3d41c0aa3778b
tranvo <vongocbaotran754@gmail.com>
2025-05-03 22:25:12 +0700

    update product.php

--- config/config.php
+++ config/config.php
@@ -1,10 +1,6 @@
 <?php
-
 define("DB_HOST", 'yamanote.proxy.rlwy.net');
 define("DB_USER", 'root');
 define("DB_PASS", 'AFVjFMeyKYycTgZIiGbJfIMjhFctakZp'); 
 define("DB_NAME", 'railway');
 define("DB_PORT", 34576);
-
-?>
-
