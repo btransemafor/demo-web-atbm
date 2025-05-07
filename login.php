24c6ebd392f9d58f3f6707552552896bb73b79e6
tranvo <vongocbaotran754@gmail.com>
2025-05-07 16:37:13 +0700

    update login

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(E_ALL);
+/////ini_set('display_errors', 0);
+//ini_set('display_startup_errors', 0);
+//error_reporting(E_ALL);
 include_once 'lib/session.php';
 Session::init(); 
 include_once 'lib/database.php';
